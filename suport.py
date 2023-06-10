394b58cb46ce849973c5d162706adbb7aae33f4f
matheus <matheusim3p@hotmail.com>
2023-06-10 15:33:40 -0300

    v2

--- suport.py
+++ suport.py
@@ -4,65 +4,58 @@ import speech_recognition as sr
 import os
 import subprocess
 
-class Suport:
+class Support:
     def __init__(self):
-     pass
-    def create_audio(self, audio, msn):
+        pass
+    
+    def create_audio(self, audio, message):
         try:
             self.audio = audio
-            self.mensager= msn
-            g_tts = gTTS(self.mensager, lang="pt-br")
+            self.message = message
+            g_tts = gTTS(self.message, lang="pt-br")
             g_tts.save(self.audio)
             playsound(self.audio)
-          
         except:
             pass
-            
-
+    
     def monitor_audio(self):
-                recog = sr.Recognizer()
-                with sr.Microphone() as mc:
-
-                    while True:
-
-                        print("Aguardando Comando")
-                        audio = recog.listen(mc)
-                        
-                        try: 
-                            self.mensager = recog.recognize_google(audio, language='pt-br')
-                            self.mensager = self.mensager.lower()
-                            self.create_audio(self.mensager)
-                            print(self.mensager)
-                            return self.mensager
-                                             
-
-                        except sr.UnknownValueError:
-                            pass
-
-    def comand(self):
-
+        recognizer = sr.Recognizer()
+        with sr.Microphone() as mc:
+            while True:
+                print("Waiting for command")
+                audio = recognizer.listen(mc)
+                
+                try:
+                    self.message = recognizer.recognize_google(audio, language='pt-br')
+                    self.message = self.message.lower()
+                    self.create_audio(self.message)
+                    print(self.message)
+                    return self.message
+                except sr.UnknownValueError:
+                    pass
+    
+    def command(self):
         try:
             self.monitor_audio()
-            if self.mensager == 'abrir league of legends' or self.mensager == 'abrir lolzinho':
-                    subprocess.run('"D:/riot/Riot Games/League of Legends/LeagueClient.exe"')
-                    print("Abrindo LoLzinho")
-                    try: os.remove("mensagem.mp4")
-                    except:
-                        pass
-                
-            elif self.mensager == 'abrir google' or self.mensager == 'abra o google':
-                    subprocess.run('"C:\Program Files\Google\Chrome\Application\chrome.exe"')
-                    print("Abrindo o Google")
-                    try: os.remove("mensagem.mp4")
-                    except:
-                        pass
+            if self.message == 'open league of legends' or self.message == 'open lol':
+                subprocess.run('"D:/riot/Riot Games/League of Legends/LeagueClient.exe"')
+                print("Opening League of Legends")
+                try:
+                    os.remove("message.mp4")
+                except:
+                    pass
+            elif self.message == 'open google':
+                subprocess.run('"C:\Program Files\Google\Chrome\Application\chrome.exe"')
+                print("Opening Google")
+                try:
+                    os.remove("message.mp4")
+                except:
+                    pass
         except:
-             pass
-        
+            pass
+    
     def main(self):
         while True:
-            self.comand()
+            self.command()
 
-Suport().main()
-
-
+Support().main()
